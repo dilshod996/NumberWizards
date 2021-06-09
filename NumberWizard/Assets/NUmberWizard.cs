256ff3ba8292a09dc8fac133138c866dc6117f15
dilshod996 <dnurmamatoff@gmail.com>
2021-06-10 02:02:35 +0900

    cool

---
+++ NumberWizard/Assets/NUmberWizard.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NUmberWizard : MonoBehaviour
+{
+    int max;
+    int min;
+    int guess;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Welcome to my world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
